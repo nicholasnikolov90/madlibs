b654ceb299f8371c9a5d06c22620fd73e5f8db17
Nick <nicknikolov7@gmail.com>
2022-12-12 20:53:27 -0700

    Added string concatenation functionality to madlib game

---
+++ madlib.py
@@ -0,0 +1,15 @@
+
+adj1 = input("Adjective: ")
+noun1 = input("Noun: ")
+verb = input("Verb: ")
+noun = input("Noun: ")
+
+madlib1 = f"With this {adj1} project, my goal is to convince hiring managers I am \
+    not an AI. I don't know if I'm doing a good job. Hopefully this {} practice project \
+        will prove I am not a {noun}"
+
+
+omputer programming is so {adj1}! This is a {adj} string concatenation \
+    practice project.  I like to {verb} fast "
+
+print(madlib1)
